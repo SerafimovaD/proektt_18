b51188677d06c6027298468c7357ad2684a798fb
SerafimovaD <daci.serafimova@gmail.com>
2023-12-22 10:10:44 +0100

    Add first java program

---
+++ src/Studentt.java
@@ -0,0 +1,12 @@
+//A Java class which has only setter methods.  
+public class Studentt {  
+	//private data member  
+	private String college;  
+	//getter method for college  
+	public void setCollege(String college){  
+	this.college=college;  
+	
+	}  
+	}  
+
+
